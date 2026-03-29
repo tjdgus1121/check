8186554f4b408c457913d4f648f821a375a39667
ChoboCode <93236905+ChoboCode@users.noreply.github.com>
2026-03-30 01:03:30 +0900

    Add files via upload

---
+++ check-sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'yaja-v1';
+const ASSETS = [
+  './check.html',
+  './faviconlogo.png',
+  './favicon.ico',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// 네트워크 우선, 실패 시 캐시 사용 (항상 최신 데이터)
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
